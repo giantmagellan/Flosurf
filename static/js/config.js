cbc0e873b78f4f1f8caf664f6df564c0ad9931ef
giantmagellan <51727942+giantmagellan@users.noreply.github.com>
2020-06-14 22:03:22 -0700

    lat-lng + params

--- static/js/config.js
+++ static/js/config.js
@@ -1,1 +1,3 @@
-const api_key = "2f05e490-ae02-11ea-9fc5-0242ac130002-2f05e544-ae02-11ea-9fc5-0242ac130002"
+const apiKey_sg = "2f05e490-ae02-11ea-9fc5-0242ac130002-2f05e544-ae02-11ea-9fc5-0242ac130002"
+
+const apiKey_mapbox = "pk.eyJ1IjoiZ2lhbnRtYWdlbGxhbiIsImEiOiJjazgxdmVhd24wYjEwM3Nud3N6b3F4aHpuIn0.XbKDOBDGgigHCefeHFnOzg";
--- static/js/surfReport.js
+++ static/js/surfReport.js
@@ -3,17 +3,35 @@
 // --------------------------------------
 
 // storm glass api endpoint url
-url = "https://api.stormglass.io/v2"
+urlWeather = "https://api.stormglass.io/v2/weather/point";
+urlTide = "https://api.stormglass.io/v2/tide/sea-level/point";
+urlAstronomy = "https://api.stormglass.io/v2/astronomy/point";
+
+const urls = 'urlWeather, urlTide, urlAstronomy';
+
+// lat-lng coordinates
+const lat = 32.7707;
+const lng = 117.2514;
+// Parameters
+const params = 'waveHeight,waveDirection,wavePeriod,windSpeed,windDirection,swellHeight,swellDirection,swellPeriod,waterTemperature,currentDirection';
+
+fetch(`https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${params}`, {
+  headers: {
+    'Authorization': '2f05e490-ae02-11ea-9fc5-0242ac130002-2f05e544-ae02-11ea-9fc5-0242ac130002'
+  }
+}).then((response) => response.json()).then((jsonData) => {
+  // Do something with response data.
+});
 
 // D3 GET request to the query url
-d3.json(url, function(data) {
+d3.json(urlWeather, function(dataWeather) {
     // Sends data.features object to the createFeatures function
-    createFeatures(data.features);
+    createFeatures(dataWeather.data);
     });
 
-// --------------------------------------
-// Features and Popups for Surf Locations
-// --------------------------------------
+// // --------------------------------------
+// // Features and Popups for Surf Locations
+// // --------------------------------------
 
 function createFeatures(surfData) {
 
